8fda30ab0cc0bc1898cc8e205c311f8a5a828465
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2018-09-05 23:15:17 +0900

    refactor

--- .emacs.d/.cask/24.5/bootstrap/commander-20140120.1052/commander-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; commander-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("commander.el") (22685 51395 624736 219000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; commander-autoloads.el ends here
--- .emacs.d/.cask/24.5/elpa/seq-2.19/seq-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; seq-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("seq-24.el" "seq-25.el" "seq-pkg.el" "seq.el")
-;;;;;;  (22685 51432 316706 13000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; seq-autoloads.el ends here
