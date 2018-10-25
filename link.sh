00f0f6cf7518acb9ba958f4bc81dfc8de36babce
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2018-10-26 00:16:07 +0900

    update

---
+++ link.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -eu
+
+ln -sf $PWD/.gvimrc ~
+ln -sf $PWD/.vimrc ~
+ln -sf $PWD/.emacs.d ~
+ln -sf $PWD/.spacemacs ~
+ln -sf $PWD/.zshrc ~
+ln -sf $PWD/.oh-my-zsh ~
+ln -sf $PWD/.tmux.conf ~
