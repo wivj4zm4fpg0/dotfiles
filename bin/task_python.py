7bfbaf7c18add4af5f299b8d371a2c5391d880fa
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2020-06-03 15:19:10 +0900

    update

---
+++ bin/task_python.py
@@ -0,0 +1,22 @@
+import json
+import os
+import re
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    json_dict = json.load(f)
+
+for command in json_dict.keys():
+    args = ''
+    command_ = json_dict[command]
+    for arg in command_['args'].keys():
+        args = '{} {} {}'.format(args, arg,
+                                 re.sub(r'[[,\'\]]', '', str(command_['args'][arg])))
+    print('python {} {}'.format(
+        command_['input'],
+        args
+    ))
+    os.system('python {} {}'.format(
+        command_['input'],
+        args
+    ))
