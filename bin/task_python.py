4a4516a210cc9a27062844ff2d1cb9cbc32488cf
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2020-06-03 15:34:03 +0900

    update

--- bin/task_python.py
+++ bin/task_python.py
@@ -3,20 +3,17 @@ import os
 import re
 import sys
 
-with open(sys.argv[1], 'r') as f:
-    json_dict = json.load(f)
+json_dict = json.load(open(sys.argv[1], 'r'))
+if len(sys.argv) == 3 and sys.argv[2] == 'no_exe':
+    do_exe = False
+else:
+    do_exe = True
 
 for command in json_dict.keys():
     args = ''
     command_ = json_dict[command]
     for arg in command_['args'].keys():
-        args = '{} {} {}'.format(args, arg,
-                                 re.sub(r'[[,\'\]]', '', str(command_['args'][arg])))
-    print('python {} {}'.format(
-        command_['input'],
-        args
-    ))
-    os.system('python {} {}'.format(
-        command_['input'],
-        args
-    ))
+        args = '{} {} {}'.format(args, arg, re.sub(r'[[,\'\]]', '', str(command_['args'][arg])))
+    print(f'python {command_["input"]} {args}')
+    if do_exe:
+        os.system(f'python {command_["input"]} {args}')
